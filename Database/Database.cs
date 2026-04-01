27dc9810363fd3726499dae0cd2077769ef795ce
AlexWybranski <alexwybranski222@gmail.com>
2026-04-01 17:19:10 +0200

    Initial commit

---
+++ Database/Database.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projekt.Items;
+using Console = Projekt.Items.Console;
+
+namespace Projekt.Database;
+
+public class Database : DbContext
+{
+    public Database(DbContextOptions<Database> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Console> Consoles { get; set; }
+    public DbSet<Accessory> Accessories { get; set; }
+    public DbSet<Game> Games { get; set; }
+    public DbSet<Wishlist> Wishlist { get; set; }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+﻿## Game & Console Collection Manager
+Web application built in C# and Blazor framework.
+
+### Main functionalities
+- Ability to add games, consoles and accessories to list
+- Automatic value check function
+- Automatic transfer for games from wishlist
